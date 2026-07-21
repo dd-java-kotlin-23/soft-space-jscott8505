86f2ad6445b07c11696466c72c9c2e5cebb5409d
jazz <jscott85@student.cnm.edu>
2026-07-21 14:20:37 -0600

    Define Room entities for `Event` and `Comment` models.

--- app/src/main/java/edu/cnm/deepdive/softspace/model/entity/Comment.kt
+++ app/src/main/java/edu/cnm/deepdive/softspace/model/entity/Comment.kt
@@ -1,5 +1,37 @@
 package edu.cnm.deepdive.softspace.model.entity
 
-class Comment {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(
+   tableName = "comment",
+   foreignKeys = [ForeignKey(
+      entity = Post::class,
+      parentColumns = ["id"],
+      childColumns = ["postId"],
+      onDelete = ForeignKey.CASCADE
+   )],
+   indices = [Index(value = ["postId"])]
+)
+ class Comment {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "comment_id")
+    val id: Long = 0
+
+   @ColumnInfo(name = "post_id")
+    val postId: Long = 0
+
+   @ColumnInfo(name = "author_name")
+    val authorName: String = ""
+    val text: String = ""
+    val created: Date = Date()
+
+   @ColumnInfo(name = "user_profile_id")
+   val userProfileId: Long? = null
 
 }
--- app/src/main/java/edu/cnm/deepdive/softspace/model/entity/Event.kt
+++ app/src/main/java/edu/cnm/deepdive/softspace/model/entity/Event.kt
@@ -1,5 +1,29 @@
 package edu.cnm.deepdive.softspace.model.entity
 
-class Event {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "events")
+ class Event {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "event_id")
+    val id: Long = 0
+
+    @ColumnInfo(index = true)
+    val title: String = ""
+    val description: String = ""
+    val location: String = ""
+
+    @ColumnInfo(name = "start_time")
+    val startTime: Date = Date()
+
+    @ColumnInfo(name = "end_time")
+    val endTime: Date = Date()
+
+    @ColumnInfo(name = "is_sensory_friendly")
+    val isSensoryFriendly: Boolean = true
 
 }
