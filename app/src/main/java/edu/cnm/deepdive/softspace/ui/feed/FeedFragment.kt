588a80deb47c528f4fc6b79f040f1c92ada40d3f
Chris Hughes <chris@speedycomputing.com>
2026-08-04 09:58:08 -0600

    bare bones saving of posts

--- app/src/main/java/edu/cnm/deepdive/softspace/ui/feed/FeedFragment.kt
+++ app/src/main/java/edu/cnm/deepdive/softspace/ui/feed/FeedFragment.kt
@@ -5,13 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
 import edu.cnm.deepdive.softspace.R
 import edu.cnm.deepdive.softspace.databinding.FragmentFeedBinding
+import edu.cnm.deepdive.softspace.model.entity.Post
 import edu.cnm.deepdive.softspace.viewmodel.FeedViewModel
 
+@AndroidEntryPoint
 class FeedFragment : Fragment() {
+
+    private val viewModel: FeedViewModel by viewModels()
 
     private var _binding: FragmentFeedBinding? = null
     private val binding: FragmentFeedBinding
@@ -34,9 +39,15 @@ class FeedFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.postsRecyclerView.adapter = postAdapter
 
-        val viewModel = ViewModelProvider(this).get(FeedViewModel::class.java)
         viewModel.allPosts().observe(viewLifecycleOwner) { posts ->
             postAdapter.submitList(posts)
+        }
+
+        binding.buttonPost.setOnClickListener {
+            val post = Post(
+                content = binding.postContentInput.text.toString()
+            )
+            viewModel.create(post)
         }
 
         binding.btnSettings.setOnClickListener {
